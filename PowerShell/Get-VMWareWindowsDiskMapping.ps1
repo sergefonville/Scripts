60f5c5330ebf0d99cb81b0f9d21172a4473c74fb
sergefonville <SFONVIL80@LT1689.future.lan>
2015-11-10 13:42:27 +0100

    Corrected Get-VMWareWindowsDiskMapping
    The situation when no powerd on VM is returned when running Get-VM is
    now handled with an error.

--- PowerShell/Get-VMWareWindowsDiskMapping.ps1
+++ PowerShell/Get-VMWareWindowsDiskMapping.ps1
@@ -2,11 +2,18 @@ Param(
 	[Parameter(Mandatory=$true)]
 	[String[]]$Server
   , [Parameter(Mandatory=$true)]
-	[String]$VM
-)
+	[String]$VMName
 
-$VMHardDisks = Get-VM -Name $VM -Server $Server | Get-HardDisk | Select-Object Name, @{Name='Uuid';Expression={$_.ExtensionData.Backing.Uuid}}
-$WinHardDisks = Get-WmiObject -Class Win32_DiskDrive -ComputerName $VM -Property SerialNumber,DeviceId | Select-Object @{Name='Disk';Expression={$_.DeviceId -Replace '.*?([0-9]+)$','Disk $1'}},@{Name='SerialNumber';Expression={[Guid]$_.SerialNumber}}
+)
+Connect-VIServer -Server $Server | Out-Null
+$VM = Get-VM -Name $VMName -Server $Server | Where-Object {$_.PowerState -eq 'PoweredOn'}
+If($VM -eq $null) {
+	Write-Error -Message $("No VM named {0} is powered on" -f  $VMName)
+	Exit
+}
+
+$VMHardDisks = Get-VM -Name $VMName -Server $Server | Get-HardDisk | Select-Object Name, @{Name='Uuid';Expression={$_.ExtensionData.Backing.Uuid}}
+$WinHardDisks = Get-WmiObject -Class Win32_DiskDrive -ComputerName $VMName -Property SerialNumber,DeviceId | Select-Object @{Name='Disk';Expression={$_.DeviceId -Replace '.*?([0-9]+)$','Disk $1'}},@{Name='SerialNumber';Expression={[Guid]$_.SerialNumber}}
 ForEach($VMHardDisk in $VMHardDisks) {
 	$WinHardDisk = $WinHardDisks | ?{$_.SerialNumber -eq $VMHardDisk.Uuid}
 	$Result = New-Object PsObject
